a19051d2ae10bcae09727c65f379fe7a1056f5db
露草くれよん <tsuyukusado@gmail.com>
2026-03-05 03:34:33 +0900

    fonts導入

---
+++ README.md
@@ -0,0 +1,3 @@
+# 各種ファイルの役割
+main.dart: 本体
+pubspec.yaml: ライブラリの読み込み
--- src/main.dart
+++ src/main.dart
@@ -1,5 +1,31 @@
-// main.dart
+import 'dart:io';
+import 'package:pdf/widgets.dart' as pw;
 
-void main() {
-  print('Hello World');
+void main() async {
+  final pdf = pw.Document();
+
+  // 日本語フォントを読み込む
+  final fontData = await File('fonts/ShipporiMincho-Regular.ttf').readAsBytes();
+  final ttf = pw.Font.ttf(fontData.buffer.asByteData());
+
+  final inputFile = File('novel/00_tsukuritai.md');
+  final content = await inputFile.readAsString();
+  final sections = content.split('===page===');
+
+  for (final section in sections) {
+    if (section.trim().isEmpty) continue;
+    pdf.addPage(
+      pw.MultiPage(
+        theme: pw.ThemeData.withFont(base: ttf),
+        build: (context) => section.trim().split(RegExp(r'\r?\n')).map((line) {
+          return pw.Text(line);
+        }).toList(),
+      ),
+    );
+  }
+
+  final file = File('test.pdf');
+  await file.writeAsBytes(await pdf.save());
+  
+  print('PDFができたぜ！');
 }
