4f16985af4c1553b20f0c81994360ecacc5f3516
露草くれよん <tsuyukusado@gmail.com>
2026-03-05 03:47:57 +0900

    行頭スペース反映

--- src/main.dart
+++ src/main.dart
@@ -8,6 +8,7 @@ void main() async {
   final fontData = await File('fonts/ShipporiMincho-Regular.ttf').readAsBytes();
   final ttf = pw.Font.ttf(fontData.buffer.asByteData());
 
+  const fontSize = 12.0;
   final inputFile = File('novel/00_tsukuritai.md');
   final content = await inputFile.readAsString();
   final sections = content.split('===page===');
@@ -17,7 +18,20 @@ void main() async {
     pdf.addPage(
       pw.MultiPage(
         theme: pw.ThemeData.withFont(base: ttf),
-        build: (context) => section.trim().split(RegExp(r'\r?\n')).map((line) {
+        build: (context) => section.split(RegExp(r'\r?\n')).map((line) {
+          final match = RegExp(r'^(\u3000+)').firstMatch(line);
+          if (match != null) {
+            final spaceCount = match.group(1)!.length;
+            final text = line.substring(match.group(1)!.length);
+            return pw.RichText(
+              text: pw.TextSpan(
+                children: [
+                  pw.WidgetSpan(child: pw.SizedBox(width: fontSize * spaceCount)),
+                  pw.TextSpan(text: text),
+                ],
+              ),
+            );
+          }
           return pw.Text(line);
         }).toList(),
       ),
